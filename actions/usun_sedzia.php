6bd25d6a3ba969ca89111ed9c827890bfcff5dc6
Julia K <owcachix@gmail.com>
2026-03-09 17:49:05 +0100

    Update

---
+++ actions/usun_sedzia.php
@@ -0,0 +1,13 @@
+<?php
+require_once "../includes/database.php";
+
+if(isset($_POST['sedzia_id'])) {
+    $sedzia_ids = $_POST['sedzia_id'];
+    foreach($sedzia_ids as $id) {
+        $sql="DELETE FROM sedzia WHERE id = $id";
+        mysqli_query($con, $sql);
+    }
+}
+mysqli_close($con);
+header("Location: ../index.html?tab=tabs%2Fsedziowie.php");
+?>
