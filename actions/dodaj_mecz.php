1ed2d60218f34a0ce0344c1a232a2964559a12ed
Julia K <owcachix@gmail.com>
2026-03-11 17:22:44 +0100

    Add files via upload

---
+++ actions/dodaj_mecz.php
@@ -0,0 +1,15 @@
+<?php
+if(isset($_POST['druzyna_1']) && isset($_POST['druzyna_2']) && isset($_POST['turniej']) && isset($_POST['sedzia']) && isset($_POST['sedzia_asystent_1']) && isset($_POST['sedzia_asystent_2'])){
+    require_once "../includes/database.php";
+    $druzyna_1 = $_POST['druzyna_1'];
+    $druzyna_2 = $_POST['druzyna_2'];
+    $turniej = $_POST['turniej'];
+    $sedzia = $_POST['sedzia'];
+    $sedzia_asystent_1 = $_POST['sedzia_asystent_1'];
+    $sedzia_asystent_2 = $_POST['sedzia_asystent_2'];
+    $sql="INSERT INTO mecz (druzyna_1, druzyna_2, turniej, sedzia, sedzia_asystent_1, sedzia_asystent_2) VALUES ('$druzyna_1', '$druzyna_2', '$turniej', '$sedzia', '$sedzia_asystent_1', '$sedzia_asystent_2')";
+    mysqli_query($con,$sql);
+    mysqli_close($con);
+}
+header("Location: ../index.html?tab=tabs%2Fmecze.php");
+?>
