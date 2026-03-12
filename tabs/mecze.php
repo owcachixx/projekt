e93be5bdc024d1ca9bf43ef96fef9ada0fbaabed
Julia K <owcachix@gmail.com>
2026-03-12 17:35:40 +0100

    Update mecze.php

--- tabs/mecze.php
+++ tabs/mecze.php
@@ -1,3 +1,12 @@
+<?php
+    require_once "../includes/database.php";
+    $sedziowie=[];
+    $sql="SELECT * FROM sedzia";
+    $query=mysqli_query($con,$sql);
+    while($row=mysqli_fetch_assoc($query)){
+        $sedziowie[]=$row;
+    }
+?>
 <section>
     <button id="dodaj_mecz_btn" onclick="dodajMecz()">Dodaj mecz</button>
     <button id="usun_mecz_btn" onclick="usunMecz()">Usuń mecz</button>
@@ -6,59 +15,70 @@
     <form method="post" action="../actions/dodaj_mecz.php" id="dodaj_mecz_form" style="display: none;">
         <select name="druzyna_1" id="druzyna_1_select">
             <?php
-                require_once "../includes/database.php";
-                $sql="SELECT * FROM druzyna WHERE id NOT IN (SELECT druzyna_1 FROM mecz UNION SELECT druzyna_2 FROM mecz)";
-                $query=mysqli_query($con,$sql);
-                while($row=mysqli_fetch_assoc($query)){
-                    echo "<option value='$row[id]'>$row[nazwa]</option>";
+                if(!isset($_SESSION['turniej_id'])){
+                    echo "<option disabled>Brak wybranego turnieju</option>";
+                }else{
+                    $sql="SELECT * FROM udzial JOIN druzyna ON udzial.id_druzyna = druzyna.id WHERE id_turniej = ".$_SESSION['turniej_id'];
+                    $query=mysqli_query($con,$sql);
+                    if(!$query){
+                        error_log("Błąd zapytania: " . mysqli_error($con));
+                    }else{
+                        while($row=mysqli_fetch_assoc($query)){
+                            $sql2="SELECT * FROM druzyna WHERE id = $row[id_druzyna]";
+                            $query2=mysqli_query($con,$sql2);
+                            $druzyna=mysqli_fetch_assoc($query2);
+                            echo "<option value='$row[id]'>$druzyna[nazwa]</option>";
+                        }
+                    }
                 }
             ?>
         </select>
         <select name="druzyna_2" id="druzyna_2_select">
             <?php
-                require_once "../includes/database.php";
-                $sql="SELECT * FROM udzial JOIN druzyna ON udzial.id_druzyna = druzyna.id WHERE id_turniej = ".$_SESSION['turniej_id'];
-                $query=mysqli_query($con,$sql);
-                while($row=mysqli_fetch_assoc($query)){
-                    echo "<option value='$row[id]'>$row[nazwa]</option>";
+                if(!isset($_SESSION['turniej_id'])){
+                    echo "<option disabled>Brak wybranego turnieju</option>";
+                }else{
+                    $sql="SELECT * FROM udzial JOIN druzyna ON udzial.id_druzyna = druzyna.id WHERE id_turniej = ".$_SESSION['turniej_id'];
+                    $query=mysqli_query($con,$sql);
+                    if(!$query){
+                        error_log("Błąd zapytania: " . mysqli_error($con));
+                    }else{
+                        while($row=mysqli_fetch_assoc($query)){
+                            $sql2="SELECT * FROM druzyna WHERE id = $row[id_druzyna]";
+                            $query2=mysqli_query($con,$sql2);
+                            $druzyna=mysqli_fetch_assoc($query2);
+                            echo "<option value='$row[id]'>$druzyna[nazwa]</option>";
+                        }
+                    }
                 }
-                mysqli_close($con);
             ?>
         </select>
         <select name="sedzia" id="sedzia_select">
-            <?php
-                require_once "../includes/database.php";
-                $sql="SELECT * FROM sedzia";
-                $query=mysqli_query($con,$sql);
-                while($row=mysqli_fetch_assoc($query)){
-                    echo "<option value='$row[id]'>$row[imie] $row[nazwisko]</option>";
-                }
-                mysqli_close($con);
-            ?>
+            <?php foreach($sedziowie as $row): ?>
+                <option value="<?= $row['id'] ?>"><?= $row['imie'] . ' ' . $row['nazwisko'] ?></option>
+            <?php endforeach; ?>
         </select>
         <select name="sedzia_asystent_1" id="sedzia_asystent_1_select">
-            <?php
-                require_once "../includes/database.php";
-                $sql="SELECT * FROM sedzia";
-                $query=mysqli_query($con,$sql);
-                while($row=mysqli_fetch_assoc($query)){
-                    echo "<option value='$row[id]'>$row[imie] $row[nazwisko]</option>";
-                }
-                mysqli_close($con);
-            ?>
+            <?php foreach($sedziowie as $row): ?>
+                <option value="<?= $row['id'] ?>"><?= $row['imie'] . ' ' . $row['nazwisko'] ?></option>
+            <?php endforeach; ?>
         </select>
         <select name="sedzia_asystent_2" id="sedzia_asystent_2_select">
-            <?php
-                require_once "../includes/database.php";
-                $sql="SELECT * FROM sedzia";
-                $query=mysqli_query($con,$sql);
-                while($row=mysqli_fetch_assoc($query)){
-                    echo "<option value='$row[id]'>$row[imie] $row[nazwisko]</option>";
-                }
-                mysqli_close($con);
-            ?>
+            <?php foreach($sedziowie as $row): ?>
+                <option value="<?= $row['id'] ?>"><?= $row['imie'] . ' ' . $row['nazwisko'] ?></option>
+            <?php endforeach; ?>
         </select>
         <button type="submit">Dodaj</button>
+    </form>
+    <form method="post" action="../actions/generuj_mecze.php" id="generuj_mecze_form" style="display: none;">
+        <input type="hidden" name="turniej_id" value="<?php echo $_SESSION['turniej_id']; ?>">
+        <input type="number" name="liczba_meczy_jednoczesnie" placeholder="Liczba meczy jednocześnie" min="1" max="10" required>
+        <select name="sedzia" id="generuj_sedzia_select" multiple>
+            <?php foreach($sedziowie as $row): ?>
+                <option value="<?= $row['id'] ?>"><?= $row['imie'] . ' ' . $row['nazwisko'] ?></option>
+            <?php endforeach; ?>
+        </select>
+        <button type="submit">Generuj</button>
     </form>
 </section>
 <section>
@@ -73,29 +93,30 @@
             <th>Sędzia asystujący 2</th>
         </tr>
     <?php
-        require_once "../includes/database.php";
         $sql="SELECT mecz.id, d1.nazwa AS druzyna_1, d2.nazwa AS druzyna_2, mecz.wynik_druzyna_1, mecz.wynik_druzyna_2, s.imie AS sedzia_imie, s.nazwisko AS sedzia_nazwisko, sa1.imie AS asystent1_imie, sa1.nazwisko AS asystent1_nazwisko, sa2.imie AS asystent2_imie, sa2.nazwisko AS asystent2_nazwisko FROM mecz INNER JOIN druzyna d1 ON mecz.druzyna_1 = d1.id INNER JOIN druzyna d2 ON mecz.druzyna_2 = d2.id INNER JOIN sedzia s ON mecz.sedzia = s.id INNER JOIN sedzia sa1 ON mecz.sedzia_asystent_1 = sa1.id INNER JOIN sedzia sa2 ON mecz.sedzia_asystent_2 = sa2.id";
         $query=mysqli_query($con, $sql);
-        while($row=mysqli_fetch_assoc($query)){
-            echo "<tr>
-            <td><input type='checkbox' class='mecz_checkbox' id='mecz_id_checkbox_$row[id]' name='mecz_id[]' value='$row[id]' style='display: none;'/>
-            <td><input type='radio' class='mecz_radio' id='mecz_id_$row[id]' name='mecz_id' value='$row[id]' style='display: none;'/>
-            <label>$row[druzyna_1]
-            $row[druzyna_1]</td>
-            <td>$row[druzyna_2]</td>
-            <td>$row[wynik_druzyna_1] - $row[wynik_druzyna_2]</td>
-            <td>$row[sedzia_imie] $row[sedzia_nazwisko]</td>
-            <td>$row[asystent1_imie] $row[asystent1_nazwisko]</td>
-            <td>$row[asystent2_imie] $row[asystent2_nazwisko]</td>
-            </label>
-            </tr>";
+        if(!$query){
+            error_log("Błąd zapytania: " . mysqli_error($con));
+        }else{
+            while($row=mysqli_fetch_assoc($query)){
+                echo "<tr>
+                <td><input type='checkbox' class='mecz_checkbox' id='mecz_id_checkbox_$row[id]' name='mecz_id[]' value='$row[id]' style='display: none;'/>
+                <input type='radio' class='mecz_radio' id='mecz_id_$row[id]' name='mecz_id' value='$row[id]' style='display: none;' data-druzyna-1='$row[druzyna_1]' data-druzyna-2='$row[druzyna_2]' data-sedzia-id='$row[sedzia_id]' data-asystent1-id='$row[asystent1_id]' data-asystent2-id='$row[asystent2_id]'/>
+                <label for='mecz_id_$row[id]'>$row[druzyna_1]</label></td>
+                <td><label for='mecz_id_$row[id]'>$row[druzyna_2]</label></td>
+                <td><label for='mecz_id_$row[id]'>$row[wynik_druzyna_1] - $row[wynik_druzyna_2]</label></td>
+                <td><label for='mecz_id_$row[id]'>$row[sedzia_imie] $row[sedzia_nazwisko]</label></td>
+                <td><label for='mecz_id_$row[id]'>$row[asystent1_imie] $row[asystent1_nazwisko]</label></td>
+                <td><label for='mecz_id_$row[id]'>$row[asystent2_imie] $row[asystent2_nazwisko]</label></td>
+                </tr>";
+            }
         }
-        mysqli_close($con);
     ?>
     </table>
     <button type="reset" id="reset_mecz" style="display: none;">Reset</button>
     <button type="submit" id="submit_usun_mecz" style="display: none;">Usuń</button>
     <button type="button" id="submit_edytuj_mecz" style="display: none;" onclick="edytujMeczForm()">Edytuj</button>
+</form>
 </section>
 <section id="edytuj_mecz_section" style="display: none;">
     <h3>Edytuj mecz</h3>
@@ -103,58 +124,44 @@
         <input type="hidden" name="mecz_id" id="edytuj_mecz_id">
         <select name="druzyna_1" id="edytuj_druzyna_1_select">
             <?php
-                require_once "../includes/database.php";
-                $sql="SELECT * FROM druzyna";
+                $sql="SELECT id, nazwa FROM druzyna";
                 $query=mysqli_query($con,$sql);
-                while($row=mysqli_fetch_assoc($query)){
-                    echo "<option value='$row[id]'>$row[nazwa]</option>";
+                if(!$query){
+                    error_log("Błąd zapytania: " . mysqli_error($con));
+                }else{
+                    while($row=mysqli_fetch_assoc($query)){
+                        echo "<option value='$row[id]'>$row[nazwa]</option>";
+                    }
                 }
-                mysqli_close($con);
             ?>
         </select>
         <select name="druzyna_2" id="edytuj_druzyna_2_select">
             <?php
-                require_once "../includes/database.php";
-                $sql="SELECT * FROM druzyna";
+                $sql="SELECT id, nazwa FROM druzyna";
                 $query=mysqli_query($con,$sql);
-                while($row=mysqli_fetch_assoc($query)){
-                    echo "<option value='$row[id]'>$row[nazwa]</option>";
+                if(!$query){
+                    error_log("Błąd zapytania: " . mysqli_error($con));
+                }else{
+                    while($row=mysqli_fetch_assoc($query)){
+                        echo "<option value='$row[id]'>$row[nazwa]</option>";
+                    }
                 }
-                mysqli_close($con);
             ?>
         </select>
         <select name="sedzia" id="edytuj_sedzia_select">
-            <?php
-                require_once "../includes/database.php";
-                $sql="SELECT * FROM sedzia";
-                $query=mysqli_query($con,$sql);
-                while($row=mysqli_fetch_assoc($query)){
-                    echo "<option value='$row[id]'>$row[imie] $row[nazwisko]</option>";
-                }
-                mysqli_close($con);
-            ?>
+            <?php foreach($sedziowie as $row): ?>
+                <option value="<?= $row['id'] ?>"><?= $row['imie'] . ' ' . $row['nazwisko'] ?></option>
+            <?php endforeach; ?>
         </select>
         <select name="sedzia_asystent_1" id="edytuj_sedzia_asystent_1_select">
-            <?php
-                require_once "../includes/database.php";
-                $sql="SELECT * FROM sedzia";
-                $query=mysqli_query($con,$sql);
-                while($row=mysqli_fetch_assoc($query)){
-                    echo "<option value='$row[id]'>$row[imie] $row[nazwisko]</option>";
-                }
-                mysqli_close($con);
-            ?>
+            <?php foreach($sedziowie as $row): ?>
+                <option value="<?= $row['id'] ?>"><?= $row['imie'] . ' ' . $row['nazwisko'] ?></option>
+            <?php endforeach; ?>
         </select>
         <select name="sedzia_asystent_2" id="edytuj_sedzia_asystent_2_select">
-            <?php
-                require_once "../includes/database.php";
-                $sql="SELECT * FROM sedzia";
-                $query=mysqli_query($con,$sql);
-                while($row=mysqli_fetch_assoc($query)){
-                    echo "<option value='$row[id]'>$row[imie] $row[nazwisko]</option>";
-                }
-                mysqli_close($con);
-            ?>
+            <?php foreach($sedziowie as $row): ?>
+                <option value="<?= $row['id'] ?>"><?= $row['imie'] . ' ' . $row['nazwisko'] ?></option>
+            <?php endforeach; ?>
         </select>
         <select name="wynik_druzyna_1" id="edytuj_wynik_druzyna_1_select">
             <?php
@@ -168,7 +175,9 @@
                 for($i=0; $i<=3; $i++){
                     echo "<option value='$i'>$i</option>";
                 }
+                mysqli_close($con);
             ?>
         </select>
         <button type="submit">Zapisz</button>
     </form>
+</section>
