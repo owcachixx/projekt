5054f0af8e61781cb3cb8a71b7ab74596c565395
Julia K <owcachix@gmail.com>
2026-03-23 21:26:43 +0100

    Add files via upload
    brakets dla drabiny turniejowej

---
+++ php/view/brackets2.php
@@ -0,0 +1,45 @@
+    <div class="bracket">
+      
+      <!-- PÓŁFINAŁ 1 -->
+      <div class="match">
+        <div class="date">23.03.2026</div>
+        <div class="team">
+          <span>Team 1</span>
+          <span class="score win">1</span>
+        </div>
+        <div class="team">
+          <span>Team 4</span>
+          <span class="score">0</span>
+        </div>
+        <div class="round">round 1</div>
+      </div>
+
+      <!-- PÓŁFINAŁ 2 -->
+      <div class="match">
+        <div class="date">23.03.2026</div>
+        <div class="team">
+          <span>Team 2</span>
+          <span class="score win">1</span>
+        </div>
+        <div class="team">
+          <span>Team 3</span>
+          <span class="score">0</span>
+        </div>
+        <div class="round">round 1</div>
+      </div>
+
+      <!-- FINAŁ -->
+      <div class="match final">
+        <div class="date">23.03.2026</div>
+        <div class="team">
+          <span><i>Team 1</i></span>
+          <span class="score">2</span>
+        </div>
+        <div class="team">
+          <span><i>Team 2</i></span>
+          <span class="score win">3</span>
+        </div>
+        <div class="round">round 2</div>
+      </div>
+
+    </div>
