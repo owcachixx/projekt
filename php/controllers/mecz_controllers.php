34c6128504b2498f170c6c3ea73b0a8c718c6f2a
Julia K <owcachix@gmail.com>
2026-03-24 20:30:52 +0100

    Update mecz_controllers.php
    dodanie do funkcji table_mecze() opcji wyświetlenia meczu kiedy jest to mecz typu BYE, usunięcie nie używanej funkcji

--- php/controllers/mecz_controllers.php
+++ php/controllers/mecz_controllers.php
@@ -56,22 +56,31 @@ function table_mecze() {
                 <input type='radio' class='mecz_radio' id='mecz_id_radio_$row[id]' name='mecz_id' value='$row[id]' style='display: none;' 
                 data-druzyna-1='$row[druzyna_1]' data-druzyna-2='$row[druzyna_2]' data-sedzia-id='$row[sedzia_id]' data-asystent1-id='$row[sedzia1_id]' data-asystent2-id='$row[sedzia2_id]' data-id='$row[id]'/>
                 <label for='mecz_id_$row[id]'>$row[druzyna_1]</label></td>
-                <td><label for='mecz_id_$row[id]'>$row[druzyna_2]</label></td>
                 <td><label for='mecz_id_$row[id]'>$row[wynik_druzyna_1] - $row[wynik_druzyna_2]</label></td>
+                <td><label for='mecz_id_$row[id]'>$row[druzyna_2]</label></td>
                 <td><label for='mecz_id_$row[id]'>$row[sedzia_imie] $row[sedzia_nazwisko]</label></td>
                 <td><label for='mecz_id_$row[id]'>$row[asystent1_imie] $row[asystent1_nazwisko]</label></td>
                 <td><label for='mecz_id_$row[id]'>$row[asystent2_imie] $row[asystent2_nazwisko]</label></td>
                 </tr>";
             }
+            $query2=pobierz_jedna_druzyna();
+            if(!$query2){
+                error_log("Błąd zapytania: ".pobierz_jedna_druzyna());
+            }else{
+                if(mysqli_num_rows($query2) !== 0){
+                $row2=mysqli_fetch_assoc($query2);
+                echo "<tr>
+                <td><input type='checkbox' class='mecz_checkbox' id='mecz_id_checkbox_$row2[id]' name='mecz_id[]' value='$row2[id]' style='display: none;'/>
+                <input type='radio' class='mecz_radio' id='mecz_id_radio_$row2[id]' name='mecz_id' value='$row2[id]' style='display: none;' 
+                data-druzyna-1='$row2[druzyna_1]' data-druzyna-2='brak' data-sedzia-id='brak' data-asystent1-id='brak' data-asystent2-id='brak' data-id='$row2[id]'/>
+                <label for='mecz_id_$row2[id]'>$row2[druzyna_1]</label></td>
+                <td><label for='mecz_id_$row2[id]'>  </label></td>
+                <td><label for='mecz_id_$row2[id]'><span class='BYE'>$row2[druzyna_2]</span></label></td>
+                </tr>";
+                }
+            }
         }
     }
 }
 
-function session_turniej() {
-    if(isset($_SESSION["turniej_id"])){
-        return $_SESSION["turniej_id"];
-    }else{
-        return;
-    }
-}
 ?>
