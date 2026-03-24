1761d2bcf7247d4b7c88acb3ca96e1f4cddae055
Julia K <owcachix@gmail.com>
2026-03-24 20:29:10 +0100

    Update drabina_controller.php
    dodanie funkcji zwracającej liczbę drużyn w turnieju

--- php/controllers/drabina_controller.php
+++ php/controllers/drabina_controller.php
@@ -2,5 +2,9 @@
 require_once __DIR__."/../models/druzyna_model.php";
 require_once __DIR__."/../models/mecz_model.php";
 
-
+function liczba_druzyn() {
+    $query=pobierz_udzial_cout_druzyny();
+    $row=mysqli_fetch_array($query);
+    return $row['liczba_druzyn'];
+}
 ?>
