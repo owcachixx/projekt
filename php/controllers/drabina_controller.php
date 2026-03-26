83d94ab16d6a4914c064c1faf43a41bba39cba8d
Julia K <owcachix@gmail.com>
2026-03-26 22:19:41 +0100

    Update drabina_controller.php
    zmiana logiki funkcji liczba_meczy()

--- php/controllers/drabina_controller.php
+++ php/controllers/drabina_controller.php
@@ -2,16 +2,16 @@
 require_once __DIR__."/../models/druzyna_model.php";
 require_once __DIR__."/../models/mecz_model.php";
 
-function liczba_druzyn() {
-    $zbior = [2,4,7,8,15,16,31,32];
-    $zbior2 = [7,15,31];
-    $query=pobierz_udzial_cout_druzyny();
+function liczba_meczy() {
+    $zbior = [4,8,16,32];
+    $query=pobierz_udzial_cout_mecz();
+    if(!$query){
+        die("SQL ERROR: " .$query);
+    }
     $row=mysqli_fetch_array($query);
-    $liczba=$row['liczba_druzyn'];
+    $liczba=$row['liczba_meczy'];
+    $liczba = $liczba * 2;
     if(in_array($liczba, $zbior)) {
-        if(in_array($liczba, $zbior2)){
-            $liczba++;
-        }
     return $liczba;
     }else{
         return "brak";
@@ -19,9 +19,11 @@ function liczba_druzyn() {
 }
 
 function bracket_include() {
-    $liczba = liczba_druzyn();
+    $liczba = liczba_meczy();
     if($liczba !== "brak"){
-        include __DIR__."/../php/view/brackets".$liczba.".php"; 
+        include __DIR__."/../view/brackets".$liczba.".php"; 
+    } else {
+        echo "Brak dopasowania";
     }
 }
 ?>
