9700b7223c93dfa5bcb1b75b277b7ec77593382a
Julia K <owcachix@gmail.com>
2026-03-26 22:17:44 +0100

    Update brackets_controller.php
    poprawa błędów składni zmiennych

--- php/controllers/brackets_controller.php
+++ php/controllers/brackets_controller.php
@@ -1,8 +1,9 @@
 <?php
-
-
+require_once __DIR__."/../models/drabina_model.php";
+require_once __DIR__."/../models/mecz_model.php";
 function bracket($runda=1,) {
-    $liczba_druzyn=liczba_druzyn();
+    $liczba_druzyn=liczba_meczy();
+    $liczba_druzyn=$liczba_druzyn*2;
     $query=get_mecz($runda);
     if(!$query){
         error_log("Błąd zapytania: ".$query);
@@ -10,7 +11,7 @@ function bracket($runda=1,) {
         if(mysqli_num_rows($query)> 0){
             while($row=mysqli_fetch_assoc($query)){
                 echo "<div class='matchup' 
-                        data-id='$row[id_meczu]'
+                        data-id='$row[id]'
                         data-score1='$row[wynik_1]'
                         data-score2='$row[wynik_2]'
                         data-team1='$row[druzyna_1]'
@@ -31,23 +32,24 @@ function bracket($runda=1,) {
                 if(!$query_bye){
                     error_log("Błąd zapytania: ".$query_bye);
                 }else{
-                    $row_bye=mysqli_fetch_assoc($query_bye);
-                    echo "<div class='matchup' 
-                            data-id='$row[id_meczu]'
-                            data-score1='$row[wynik_1]'
-                            data-score2='$row[wynik_2]'
-                            data-team1='5'
-                            data-team2='0'>
-                        <div class='participants'>
+                    while($row_bye=mysqli_fetch_assoc($query_bye)){
+                        echo "<div class='matchup' 
+                                data-id='$row_bye[id]'
+                                data-score1='5'
+                                data-score2='0'
+                                data-team1='$row_bye[wynik_1]'
+                                data-team2='$row_bye[wynik_2]'>
+                            <div class='participants'>
+                                <div class='participant'>
+                                <span>$row_bye[druzyna_1]</span>
+                                <span class='score'>$row_bye[wynik_1]</span>
+                            </div>
                             <div class='participant'>
-                            <span>$row_bye[druzyna_1]</span>
-                            <span class='score'>$row_bye[wynik_1]</span>
-                        </div>
-                        <div class='participant'>
-                            <span>$row_bye[druzyna_2]</span>
-                            <span class='score'>$row_bye[wynik_2]</span>
-                        </div>
-                    </div>";
+                                <span>$row_bye[druzyna_2]</span>
+                                <span class='score'>$row_bye[wynik_2]</span>
+                            </div>
+                        </div>";
+                    }
                 }
             }
         }else{
@@ -87,7 +89,7 @@ function generuj_runde($liczba_rund){
                                 ?>
                                 <form method="post" action="../php/actions/generuj_runde.php" id='form_generuj_runde'>
                                     <input type='hidden' value='<?= $runda; ?>' name='runda'>
-                                    <input type="hidden" name="turniej_id" value="<?php include "../php/includes/session_id.php"; ?>">
+                                    <input type="hidden" name="turniej_id" value="<?= $_SESSION['turniej_id']; ?>">
                                     <label for="liczba_meczy_jednoczesnie"><p>Liczba meczy rozgrywanych w tym samym momencie:</p></label>
                                     <input type="number" name="liczba_meczy_jednoczesnie" placeholder="Liczba meczy jednocześnie" min="1" max="10" value="1" id="liczba_meczy_jednoczesnie" required><br>
                                     <label for="generuj_sedzia_select"><p>Wybierz głównych sędziów:<br><span>(Prztyrzymaj Control by zaznaczyć więcej)</span></p></label>
@@ -98,7 +100,6 @@ function generuj_runde($liczba_rund){
                             }
                         }
                     }else{
-                        echo "Nie dokończona runda.";
                     }
                 }
             }
@@ -106,5 +107,3 @@ function generuj_runde($liczba_rund){
     }
 }
 ?>
-
-
